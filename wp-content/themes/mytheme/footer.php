9aca6d8f2c2334f0ea8e769a1c639888a32163eb
TakaTaka 0 <tk.1987.6.9+1@gmail.com>
2018-02-01 03:15:29 +0900

    Revert "first commit"
    This reverts commit dd48aa8a69e763480a091fecdbef30f5e69d0881.

--- wp-content/themes/mytheme/footer.php
+++
@@ -1,8 +0,0 @@
-<div id="footer" class="container">
-  Copyright 2017<?php if (date("Y")!=2017) {
-    echo date("-Y");
-} ?> All rigths reserved
-</div><!-- /footer -->
-<?php wp_footer(); ?>
-</body>
-</html>
